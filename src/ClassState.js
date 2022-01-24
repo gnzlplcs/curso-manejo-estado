514d1869b75dbdc15dac45a90700c29e9dd50a3e
gnzlplcs <palaciosgm@outlook.com>
2022-01-23 21:53:43 -0500

    estados compuestos con React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Loading } from './Loading';
 
+const SECURITY_CODE = 'paradigma';
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: true,
+      value: '',
+      error: false,
       loading: false,
     };
   }
@@ -18,7 +21,12 @@ class ClassState extends React.Component {
     console.log('actualización');
     if(this.state.loading) {
       setTimeout(() => {
-        this.setState(prevState => ({ loading: !prevState.loading }))
+        // this.setState(prevState => ({ loading: !prevState.loading }))
+        if(SECURITY_CODE === this.state.value) {
+          this.setState({ error: false, loading: false });
+        } else {
+          this.setState({ error: true, loading: false })
+        }
       }, 3000);
     }
   }
@@ -33,13 +41,19 @@ class ClassState extends React.Component {
       <div>
         <h2>Eliminar {this.props.name} </h2>
         <p>Por favor, escribe el código de seguridad.</p>
-        <input placeholder='Código de seguridad' />
+        <input
+          placeholder='Código de seguridad'
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value })
+          }}
+          />
         <button
           onClick={() =>
             this.setState(prevState => ({ loading: !prevState.loading }))
           }
         >Comprobar</button>
-        {this.state.error && (
+        {(this.state.error && !this.state.loading) && (
           <p>Error: el código es incorrecto</p>
         )}
         {this.state.loading && (
