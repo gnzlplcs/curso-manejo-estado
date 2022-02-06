9243b3713fb496fdf1e5420d336dc2927309f423
gnzlplcs <palaciosgm@outlook.com>
2022-02-05 20:10:49 -0500

    estados simples es React.Component y en function component (useState)

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { ClassState } from './ClassState';
 function App() {
   return (
     <div className='App'>
-      <UseState />
-      <ClassState />
+      <UseState name="UseState" />
+      <ClassState name="ClassState" />
     </div>
   );
 }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-function UseState() {
+function UseState({ name }) {
+  // agregamos un manejo de estado para el error
+  const [ error, setError ] = React.useState(false);
+
   return (
     <div>
-      <h2>Eliminar UseState</h2>
+      <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el código de seguridad.</p>
+      {error &&  (
+        <p>Error: el código es incorrecto.</p>
+      )}
       <input placeholder='Código de seguridad'/>
-      <button>Comprobar</button>
+      <button
+        onClick={() => setError(!error)}
+      >Comprobar</button>
     </div>
   );}
 
