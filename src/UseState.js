df8ff290aeb26b622afd5af58e8eea5e022b2461
gnzlplcs <palaciosgm@outlook.com>
2022-02-05 22:39:35 -0500

    estados compuestos en UseState.js

--- src/UseState.js
+++ src/UseState.js
@@ -1,47 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SECURITY_CODE = 'paradigma'
 
 function UseState({ name }) {
-  // agregamos un manejo de estado para el error
-  const [ error, setError ] = React.useState(false);
-  const [ loading, setLoading ] = React.useState(false);
-  const [ value, setValue ] = React.useState('');
-
-  console.log(value)
-
+  const [ state, setState ] = useState({
+    error: false,
+    loading: false,
+    value: '',
+  });
+  
   React.useEffect(()=> {
-    if (loading) {
+    if (state.loading) {
       setTimeout(() => {
-        if (value === SECURITY_CODE) {
-          setError(false)
+        if (state.value === SECURITY_CODE) {
+          setState({
+            ...state,
+            error: false,
+            loading: false,
+          })
         } else {
-          setError(true);
+          setState({
+            ...state,
+            error: true,
+            loading: false
+          });
         }
-        setLoading(false);
       }, 3000);
     }
-  }, [loading])
+  }, [state.loading])
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el código de seguridad.</p>
-      {(error && !loading) &&  (
+      {(state.error && !state.loading) &&  (
         <p>Error: el código es incorrecto.</p>
       )}
-      {loading &&  (
+      {state.loading &&  (
         <p>Cargando...</p>
       )}
       <input
         placeholder='Código de seguridad'
-        value={value}
+        value={state.value}
         onChange={(e) => {
-          setValue(e.target.value);
+          setState({
+            ...state,
+            value: e.target.value
+          });
         }}
       />
       <button
-        onClick={() => setLoading(true)}
+        onClick={() => setState({
+          ...state,
+          loading: true
+        })}
       >Comprobar</button>
     </div>
   );}
